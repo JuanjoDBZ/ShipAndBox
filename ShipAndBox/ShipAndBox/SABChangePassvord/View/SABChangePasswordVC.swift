d91e8da03811d3e405ac3e3677ed2fd86d6f212c
Alejandro Galindo <alejandro.galindo@isita.com.mx>
2021-06-30 17:54:25 -0500

    -Se agrega módulo de cambio de contraseña - Se agrega las imágenes que se usarán (logos, iconos etc)

---
+++ ShipAndBox/ShipAndBox/SABChangePassvord/View/SABChangePasswordVC.swift
@@ -0,0 +1,22 @@
+//
+//  SABChangePasswordVC.swift
+//  ShipAndBox
+//
+//  Created by ISITA on 30/06/21.
+//  
+//
+
+import Foundation
+import UIKit
+
+class SABChangePasswordVC: UIViewController {
+
+    var presenter: SABChangePasswordPresenterProtocol?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+///Protocolo para recibir datos de presenter.
+extension SABChangePasswordVC: SABChangePasswordViewProtocol {
+}
